392e9dafe3c0787b016e50f61ad013bc9b9cfff6
romi477 <romi4240101@gmail.com>
2022-05-07 17:10:07 +0300

    Initial commit

---
+++ toggle_report.py
@@ -0,0 +1,121 @@
+# python3
+
+from collections import defaultdict
+import logging
+import re
+
+from requests.auth import HTTPBasicAuth
+import requests
+
+
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.DEBUG)
+
+channel = logging.StreamHandler()
+formatter = logging.Formatter('\n[%(levelname)s] %(message)s')
+
+channel.setFormatter(formatter)
+_log.addHandler(channel)
+
+WORKSPACE = 0000000
+TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+HEADERS = {
+    'content-type': 'application/json',
+}
+PARAMS = {
+    'workspace_id': WORKSPACE,
+    'since': '2022-04-01',
+    'until': '2022-05-01',
+    'user_agent': 'api_test',
+}
+SUMMARY_URL = 'https://api.track.toggl.com/reports/api/v2/summary'
+
+
+def format_date(rdate):
+    year = rdate[-2:]
+    month = rdate[2:4]
+    day = rdate[:2]
+    return f'20{year}-{month}-{day}'
+
+
+def fetch_data(since, until):
+    input_params = {
+        'since': format_date(since),
+        'until': format_date(until),
+    }
+    response = requests.get(
+        SUMMARY_URL,
+        params={**PARAMS, **input_params},
+        headers=HEADERS,
+        auth=HTTPBasicAuth(TOKEN, 'api_token'),
+    )
+
+    if response.ok:
+        return response.json()
+
+    raise Exception(response.text)
+
+
+def parse_item_name(title):
+    entry_list = re.findall(r'\[(.*?)\]', title.get('time_entry', str()))
+    task_code = entry_list and entry_list[0] or str()
+    project_code = task_code.split('-')
+    project_name = project_code and project_code[0] or str()
+    return project_name.upper()
+
+
+def parse_item(item):
+    item_name = parse_item_name(item.get('title', dict()))
+    return item_name, item.get('time')
+
+
+def analyze_data(data_list):
+    data_dict = defaultdict(list)
+
+    if not data_list:
+        _log.info('Empty received data.')
+        return data_dict
+
+    for data in data_list:
+        total_time = data.get('time', int())
+        data_dict['total_time'].append(total_time)
+
+        for item in data.get('items', []):
+            item_key, item_value = parse_item(item)
+            data_dict[item_key].append(item_value)
+
+    return data_dict
+
+
+def print_data(name, vals_list):
+    seconds = sum(vals_list)
+    hours = seconds // 3600000
+    minutes = seconds % 3600000 / 60000
+    print(f'{name} - {hours}h {minutes}m')
+
+
+def _exit():
+    input('\nPress any key to exit ...')
+    exit()
+
+
+if __name__ == '__main__':
+    since = input('Start date (for ex. `010322`): ')
+    until = input('End date (for ex. `010422`): ')
+
+    try:
+        response = fetch_data(since, until)
+    except Exception as ex:
+        _log.error(ex)
+        _exit()
+
+    if not response.get('total_grand'):
+        _log.info('There was a job free period.')
+        _exit()
+
+    data_dict = analyze_data(response.get('data', []))
+
+    print('\n=======SUMMARY REPORT=======\n')
+    for key, vals_list in data_dict.items():
+        print_data(key, vals_list)
